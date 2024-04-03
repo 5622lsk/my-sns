54140abd2c6d45967fe398f5274fe1a853e1436f
임성경 <5622lsk@naver.com>
2024-04-03 23:49:54 +0900

    feat: 데이터 생성

--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { FiImage } from "react-icons/fi";
+import { collection, addDoc } from "firebase/firestore"; 
+import { db } from 'firebaseApp';
+import { toast } from 'react-toastify';
+import AuthContext from 'context/AuthContext';
+
 
 export default function PostForm() {
+  const [content, setContent] = useState<string>("")
+  const {user} = useContext(AuthContext);
   const handleFileUpload = () => {};
 
+  const onSubmit = async(e: any) => {
+    e.preventDefault(); //폼이 일단 넘어가지 않도록
+
+    try{
+      await addDoc(collection(db, 'posts'),{
+        content: content,
+        cretedAt: new Date()?.toLocaleDateString("ko", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+        uid: user?.uid,
+        email: user?.email,
+      })
+      setContent("");//성공적으로 전송되면 콘텐츠 지움
+      toast.success("게시글을 생성했습니다");
+    } catch(e: any){
+      console.log(e);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {
+      target: {name, value}
+    } = e;
+
+    if (name === "content"){
+      setContent(value);
+    }
+  };
+
   return (
-    <form className='post-form'>
+    <form className='post-form' onSubmit={onSubmit}>
       <textarea 
         className="post-form__textarea"
         name="content"
         id="content"
         placeholder='write your story'
+        onChange={onChange}
         required
       />
       <div className="post-form__submit-area">
