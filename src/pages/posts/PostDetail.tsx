6abf15445f4056c30584751de47beecc8c8b1ee2
임성경 <5622lsk@naver.com>
2024-04-12 00:56:17 +0900

    feat: 게시글 수정

--- src/pages/posts/PostDetail.tsx
+++ src/pages/posts/PostDetail.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import Loader from 'components/loader/Loader';
+import PostBox from 'components/posts/PostBox';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from 'firebaseApp';
+import { PostProps } from 'pages/home/HomePage';
+import React, { useCallback, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import { IoIosArrowBack } from "react-icons/io";
 
 export default function PostDetail() {
+  const [post, setPost] = useState<PostProps | null>(null);
+  //초기에는 포스트 정보가 없는 상태지만,데이터를 받아오면 해당 포스트 정보를 설정할 수 있음.
+  const params = useParams();
+  const navigate = useNavigate();
+
+
+  const getPost = useCallback( async() => {
+    if(params.id){
+      const docRef = doc(db, "posts", params.id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id})
+    }
+  }, [params.id])
+
+  useEffect(()=>{
+    if(params.id) getPost();
+  },[getPost, params.id])
+
+
   return (
-    <div>
-      <h1>detail</h1>
+    <div className='post'>
+      <div className='post__header'>
+        <button type="button" onClick={() => {navigate(-1)}}>
+          <IoIosArrowBack className='post__header-btn'/>
+        </button>
+      {post ? <PostBox post={post} /> : <Loader/>}
+      </div>
     </div>
   )
 }
