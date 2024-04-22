7ad9351ee638315ced6551baf8ec21e6178428b1
임성경 <5622lsk@naver.com>
2024-04-22 21:51:18 +0900

    feat: 게시글 좋아요 & 탭 구분 구현

--- src/components/posts/PostBox.tsx
+++ src/components/posts/PostBox.tsx
@@ -1,10 +1,16 @@
 import AuthContext from "context/AuthContext";
-import { deleteDoc, doc } from "firebase/firestore";
+import {
+  arrayRemove,
+  arrayUnion,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 import { db, storage } from "firebaseApp";
 import { PostProps } from "pages/home/HomePage";
 import { useContext } from "react";
-import { AiFillHeart } from "react-icons/ai";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { FaRegComment, FaUserCircle } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -16,6 +22,24 @@ interface PostBoxProps {
 export default function PostBox({ post }: PostBoxProps) {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const toggleLike = async () => {
+    const postRef = doc(db, "posts", post.id); //어떤 게시글인지
+
+    if (user?.uid && post?.likes?.includes(user?.uid)) {
+      //사용자가 좋아요를 미리 한 겨우 => 좋아요 취소
+      await updateDoc(postRef, {
+        likes: arrayRemove(user?.uid), //arrayRemove: 배열에서 자동으로 해당값 제거
+        likeCount: post?.likeCount ? post?.likeCount - 1 : 0,
+      });
+    } else {
+      //사용자가 좋아요를 하지 않은경우 => 좋아요 추가
+      await updateDoc(postRef, {
+        likes: arrayUnion(user?.uid), //arrayUnion: 배열에서 자동으로 해당값 추가
+        likeCount: post?.likeCount ? post?.likeCount + 1 : 1,
+      });
+    }
+  };
 
   const handleDelete = async () => {
     const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
@@ -88,8 +112,13 @@ export default function PostBox({ post }: PostBoxProps) {
           </>
         )}
 
-        <button type="button" className="post__likes">
-          <AiFillHeart />
+        <button type="button" className="post__likes" onClick={toggleLike}>
+          {user && post?.likes?.includes(user.uid) ? (
+            <AiFillHeart />
+          ) : (
+            <AiOutlineHeart />
+          )}
+
           {post?.likeCount || 0}
         </button>
         <button type="button" className="post__comments">
--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -14,26 +14,42 @@ import { useNavigate } from "react-router-dom";
 
 const PROFILE_DEFAULT_URL = "/logo.svg";
 
+type TabType = "my" | "like";
+
 export default function ProfilePage() {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [activeTab, setActiveTab] = useState<TabType>("my"); //디폴트는 나의글로
+  const [myposts, setMyPosts] = useState<PostProps[]>([]);
+  const [likePosts, setLikePosts] = useState<PostProps[]>([]);
   const { user } = useContext(AuthContext); //사용자가 있을때만 호출
   const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
       let postsRef = collection(db, "posts");
-      let postsQuery = query(
+      const mypostsQuery = query(
         postsRef,
         where("uid", "==", user.uid), //현재 uid가 로그인된 사용자 uid와 같을때만. 내가 쓴글만 보여주기
         orderBy("createdAt", "desc")
       );
+      const likepostsQuery = query(
+        postsRef,
+        where("likes", "array-contains", user.uid), //현재 uid가 로그인된 사용자 uid와 같을때만. 내가 쓴글만 보여주기
+        orderBy("createdAt", "desc")
+      );
 
-      onSnapshot(postsQuery, (snapShot) => {
+      onSnapshot(mypostsQuery, (snapShot) => {
         let dataObj = snapShot.docs.map((doc) => ({
           ...doc.data(),
           id: doc?.id,
         }));
-        setPosts(dataObj as PostProps[]);
+        setMyPosts(dataObj as PostProps[]);
+      });
+      onSnapshot(likepostsQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+        setLikePosts(dataObj as PostProps[]);
       });
     }
   }, [user]);
@@ -63,18 +79,45 @@ export default function ProfilePage() {
           <div className="profile__email">{user?.email || ""}</div>
         </div>
         <div className="home__tabs">
-          <div className="home__tab home__tab--active">나의 게시글</div>
-          <div className="home__tab">저장한 게시글</div>
+          <div
+            className={`home__tab ${activeTab === "my" && "home__tab--active"}`}
+            onClick={() => setActiveTab("my")}
+          >
+            나의 게시글
+          </div>
+          <div
+            className={`home__tab ${
+              activeTab === "like" && "home__tab--active"
+            }`}
+            onClick={() => {
+              setActiveTab("like");
+            }}
+          >
+            좋아요한 게시글
+          </div>
         </div>
-        <div className="post">
-          {posts?.length > 0 ? (
-            posts?.map((post) => <PostBox post={post} key={post.id} />)
-          ) : (
-            <div className="post__no-posts">
-              <div className="post__text">게시글이 없습니다.</div>
-            </div>
-          )}
-        </div>
+        {activeTab === "my" && (
+          <div className="post">
+            {myposts?.length > 0 ? (
+              myposts?.map((post) => <PostBox post={post} key={post.id} />)
+            ) : (
+              <div className="post__no-posts">
+                <div className="post__text">게시글이 없습니다.</div>
+              </div>
+            )}
+          </div>
+        )}
+        {activeTab === "like" && (
+          <div className="post">
+            {likePosts?.length > 0 ? (
+              likePosts?.map((post) => <PostBox post={post} key={post.id} />)
+            ) : (
+              <div className="post__no-posts">
+                <div className="post__text">게시글이 없습니다.</div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
